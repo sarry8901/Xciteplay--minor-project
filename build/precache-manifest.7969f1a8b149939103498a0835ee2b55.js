01fc1669f722da3a09e143797061002004e41b2e
Sarvesh Bachute <sarveshbachute@gmail.com>
2023-01-20 19:15:22 +0530

    build folder uploaded

---
+++ build/precache-manifest.7969f1a8b149939103498a0835ee2b55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f7727e0d5fd13b4d340cfde19dbe3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c57346a7d00931d820ea",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "beb6ce49e622d10eeb4c",
+    "url": "/static/css/main.fadf39d6.chunk.css"
+  },
+  {
+    "revision": "c57346a7d00931d820ea",
+    "url": "/static/js/2.90cdf720.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.90cdf720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb6ce49e622d10eeb4c",
+    "url": "/static/js/main.125ec867.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "f85d70f848d4d0a8871eaf004457c933",
+    "url": "/static/media/logo.f85d70f8.png"
+  }
+]);
